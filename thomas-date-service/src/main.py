b867851bffc3fd4e1d1f9926cd7f3b890c975f0f
Thomas Hough <thomas.hough@nhst.no>
2021-10-24 16:04:30 +0200

    Moved files

--- thomas-date-service/src/main.py
+++ thomas-date-service/src/main.py
@@ -6,11 +6,10 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/')
+@app.route('/date-service')
 def home():
     return f'''<h1>Date: {datetime.now()}</h1>'''
 
 
 if __name__ == "__main__":
     app.run()
-
