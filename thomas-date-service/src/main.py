5d86bdd23594232eb6ece45b0f027640b98e2a96
Thomas Hough <thomas.hough@nhst.no>
2021-10-24 18:41:09 +0200

    new files

--- thomas-date-service/src/main.py
+++ thomas-date-service/src/main.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from pymongo import MongoClient
+from random import randint
 import flask
 
 
@@ -6,10 +8,36 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/date-service')
+@app.route('/')
 def home():
     return f'''<h1>Date: {datetime.now()}</h1>'''
 
 
 if __name__ == "__main__":
+
+    client = MongoClient(
+        port=27017,
+        username='root',
+        password='example'
+    )
+    db = client.thho
+    names = ['Kitchen', 'Animal', 'State', 'Tastey', 'Big', 'City', 'Fish', 'Pizza', 'Goat', 'Salty', 'Sandwich',
+             'Lazy', 'Fun']
+    company_type = ['LLC', 'Inc', 'Company', 'Corporation']
+    company_cuisine = ['Pizza', 'Bar Food', 'Fast Food', 'Italian', 'Mexican', 'American', 'Sushi Bar', 'Vegetarian']
+    for x in range(1, 501):
+        business = {
+            'name': names[randint(0, (len(names) - 1))] + ' ' + names[randint(0, (len(names) - 1))] + ' ' +
+                    company_type[randint(0, (len(company_type) - 1))],
+            'rating': randint(1, 5),
+            'cuisine': company_cuisine[randint(0, (len(company_cuisine) - 1))]
+        }
+        # Step 3: Insert business object directly into MongoDB via insert_one
+        result = db.reviews.insert_one(business)
+        # Step 4: Print to the console the ObjectID of the new document
+        print('Created {0} of 500 as {1}'.format(x, result.inserted_id))
+    # Step 5: Tell us that you are done
+    print('finished creating 500 business reviews')
+
+
     app.run()
