d91b1507d856f583253d2b75e180d3e871bc2351
Thomas Hough <thomas.hough@nhst.no>
2021-10-27 10:34:20 +0200

    fixed bug

--- thomas-date-service/src/main.py
+++ thomas-date-service/src/main.py
@@ -2,6 +2,16 @@ from datetime import datetime
 from pymongo import MongoClient
 import flask
 
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/')
+def home():
+    return f'''<h1>Date: {datetime.now()}</h1>'''
+
+
 if __name__ == "__main__":
 
     client = MongoClient(
@@ -14,19 +24,12 @@ if __name__ == "__main__":
     model = ['BD-2', 'Distortion +', 'AD-9']
     pedal_type = ['Drive', 'Drive', 'Delay']
 
-    for i in range(len(brand)):
+    for i in range(0, len(brand)):
         pedal = {
             'brand': brand[i],
             'model': model[i],
             'pedal_type': pedal_type[i]
         }
-        print(brand[i])
         result = db.pedals.insert_one(pedal)
-    app = flask.Flask(__name__)
-    app.config["DEBUG"] = True
 
-
-    @app.route('/')
-    def home():
-        return f'''<h1>Date: {datetime.now()}</h1>'''
     app.run()
