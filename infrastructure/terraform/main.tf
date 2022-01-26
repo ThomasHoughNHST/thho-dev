e4d89e9d675ffc4326357291d3b45fe8e681497c
Thomas Hough <thomas.hough@nhst.no>
2022-01-26 09:18:15 +0100

    Testing access policies

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -91,53 +91,6 @@ resource "azurerm_key_vault" "keyvault" {
   }
   access_policy {
   tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-      "List",
-      "Update",
-      "Create",
-      "Import",
-      "Delete",
-      "Recover",
-      "Backup",
-      "Restore",
-      "Purge"
-    ]
-
-    secret_permissions = [
-      "Get",
-      "List",
-      "Set",
-      "Delete",
-      "Recover",
-      "Backup",
-      "Restore",
-      "Purge"
-    ]
-
-    certificate_permissions = [
-      "Get",
-      "List",
-      "Update",
-      "Create",
-      "Import",
-      "Delete",
-      "Recover",
-      "Backup",
-      "Restore",
-      "ManageContacts",
-      "ManageIssuers",
-      "GetIssuers",
-      "ListIssuers",
-      "SetIssuers",
-      "DeleteIssuers",
-      "Purge"
-    ]
-  }
-  access_policy {
-  tenant_id = data.azurerm_client_config.current.tenant_id
   object_id = "c8c603f5-95a2-4d06-8bc9-6d00c4dc325c"
 
     key_permissions = [
