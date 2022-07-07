81b988f6a77a4b6278558009c81f9cdf3d3237ac
ThomasHoughNHST <thomas.hough@nhst.no>
2022-07-07 13:12:29 +0200

    working version

--- src/testers/compare dataframes.py
+++ src/testers/compare dataframes.py
@@ -5,6 +5,9 @@ data2 = [[1, 'A1'], [2, 'B'], [3, 'C']]
 df = pd.DataFrame(data, columns=['id', 'letter'])
 df2 = pd.DataFrame(data, columns=['id', 'letter'])
 o = df.equals(df2)
+desired_width = 320
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns', 20)
 print(o)
 
 out = pd.merge(df, df2,
